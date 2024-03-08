a2b1d929a22f81ed2b772fc20125c29250868e81
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2024-03-08 23:46:44 +0400

    add swegger2

--- src/main/java/kodlama/io/hrms/HrmsApplication.java
+++ src/main/java/kodlama/io/hrms/HrmsApplication.java
@@ -1,7 +1,11 @@
 package kodlama.io.hrms;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HrmsApplication {
@@ -10,4 +14,11 @@ public class HrmsApplication {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI api() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Contact Application API")
+						.description("This is a sample Spring Boot RESTful service using springdoc-openapi and OpenAPI 3."));
+	}
 }
